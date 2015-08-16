9ae7d9bacc8005f22b56aeba84eb4b8001e050ca
bigwheel <k.bigwheel+eng@gmail.com>
2015-08-16 20:24:09 +0900

    Just after 'berks cookbook skype-daemon'

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'skype-daemon'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures skype-daemon'
+long_description 'Installs/Configures skype-daemon'
+version          '0.1.0'
